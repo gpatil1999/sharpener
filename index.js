55a43793a61d1249bacd00e0f1d1a32f01df943f
gpatil1999 <67685276+gpatil1999@users.noreply.github.com>
2021-11-04 16:02:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ function getLocalData(){
     var retrievedObject = localStorage.getItem('myObj');
     console.log(JSON.parse(retrievedObject));
 
+    var retrievedObject = JSON.parse(retrievedObject);
+    console.log(retrievedObject);
+
     var itemList = document.getElementById('items');
     
+    retrievedObject.forEach(element => {
+    
+        console.log(element);
     var li1 = document.createElement('li');
-    li1.appendChild(document.createTextNode(JSON.parse(retrievedObject).name));
+    li1.appendChild(document.createTextNode(element.name));
     itemList.appendChild(li1);
 
     var li2 = document.createElement('li');
-    li2.appendChild(document.createTextNode(JSON.parse(retrievedObject).email));
+    li2.appendChild(document.createTextNode(element.email));
     itemList.appendChild(li2);
 
     var li3 = document.createElement('li');
-    li3.appendChild(document.createTextNode(JSON.parse(retrievedObject).phone));
+    li3.appendChild(document.createTextNode(element.phone));
     itemList.appendChild(li3);
+    });
 
 }
 
@@ -24,11 +31,19 @@ function display() {
         event.preventDefault();
     });
 
-    let myObj = {
+    var values = JSON.parse(localStorage.getItem('myObj'));
+    if (values === null) {
+      values = [];
+    }
+
+    let newObj = {
         name : document.getElementById("username").value,
         email : document.getElementById("usermail").value,
         phone : document.getElementById("userphone").value
     }
 
-    localStorage.setItem("myObj", JSON.stringify(myObj));
+    values.push(newObj);
+
+    localStorage.setItem("myObj", JSON.stringify(values));
+    console.log(localStorage.getItem("myObj"));
 }
