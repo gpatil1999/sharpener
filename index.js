df312ac5e9ac7af54bbc7f144268f302da073c5e
gpatil1999 <67685276+gpatil1999@users.noreply.github.com>
2021-11-04 15:19:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
+
+function getLocalData(){
+    var retrievedObject = localStorage.getItem('myObj');
+    console.log(JSON.parse(retrievedObject));
+
+    var itemList = document.getElementById('items');
+    
+    var li1 = document.createElement('li');
+    li1.appendChild(document.createTextNode(JSON.parse(retrievedObject).name));
+    itemList.appendChild(li1);
+
+    var li2 = document.createElement('li');
+    li2.appendChild(document.createTextNode(JSON.parse(retrievedObject).email));
+    itemList.appendChild(li2);
+
+    var li3 = document.createElement('li');
+    li3.appendChild(document.createTextNode(JSON.parse(retrievedObject).phone));
+    itemList.appendChild(li3);
+
+}
+
 function display() { 
     document.addEventListener("click", function(event) {
         event.preventDefault();
     });
-    localStorage.setItem('username', document.getElementById("username").value);
-    localStorage.setItem('usermail', document.getElementById("usermail").value);
-    localStorage.setItem('userphone', document.getElementById("userphone").value);
+
+    let myObj = {
+        name : document.getElementById("username").value,
+        email : document.getElementById("usermail").value,
+        phone : document.getElementById("userphone").value
+    }
+
+    localStorage.setItem("myObj", JSON.stringify(myObj));
 }
