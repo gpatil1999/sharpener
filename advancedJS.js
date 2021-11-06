c9e16251a4cb9cf48955444aa6711c005bbf70cb
gpatil1999 <67685276+gpatil1999@users.noreply.github.com>
2021-11-06 12:33:48 +0530

    Add files via upload

---
+++ advancedJS.js
@@ -0,0 +1,18 @@
+var obj = {num : 2};
+
+var add = function(a,b,c){
+    return this.num + a+b+c;
+};
+
+var ans = add.call(obj,1,2,3);
+console.log(ans);
+
+var arr = [1,2,3];
+
+ans = add.apply(obj,arr);
+console.log(ans);
+
+
+var bound = add.bind(obj);
+ans = bound(1,2,3);
+console.log(ans);
